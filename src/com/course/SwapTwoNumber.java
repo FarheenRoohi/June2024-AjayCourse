ea843088312f67657ea0a07d4b62c3977c8858bb
FarheenRoohi <roohihabeeb04@gmail.com>
2024-06-21 07:46:57 -0500

    added ne file

---
+++ src/com/course/SwapTwoNumber.java
@@ -0,0 +1,16 @@
+package com.course;
+
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int temp= 0;
+		a=b;
+		b=temp;
+		temp=a;
+		
+        System.out.println(a+b);
+	}
+
+}
