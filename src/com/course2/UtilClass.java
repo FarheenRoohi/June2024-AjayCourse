00045d9d1cd640007088e508e94b98189386d404
FarheenRoohi <129792243+FarheenRoohi@users.noreply.github.com>
2024-06-24 19:29:06 -0500

    Update UtilClass.java

--- src/com/course2/UtilClass.java
+++ src/com/course2/UtilClass.java
@@ -3,7 +3,7 @@ package com.course2;
 public class UtilClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello+hi");
 		// we use stream to read data file
 
 	}
