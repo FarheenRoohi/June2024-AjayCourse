fc8a87c7b59a074b863f5217891efe58766547e9
FarheenRoohi <roohihabeeb04@gmail.com>
2024-06-24 23:39:35 -0500

    modified the code

---
+++ src/com/course2/NewGitCloneClass.java
@@ -0,0 +1,11 @@
+package com.course2;
+
+public class NewGitCloneClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+	}
+
+}
