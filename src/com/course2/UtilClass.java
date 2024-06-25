a868f1aadb7ae49a04c24cde2b91c3a099aefffc
FarheenRoohi <roohihabeeb04@gmail.com>
2024-06-24 19:36:23 -0500

    one file code is changed

--- src/com/course2/UtilClass.java
+++ src/com/course2/UtilClass.java
@@ -5,6 +5,7 @@ public class UtilClass {
 	public static void main(String[] args) {
 		System.out.println("hello+hi");
 		// we use stream to read data file
+		// we use stream to read data file
 
 	}
 
